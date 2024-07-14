f7b44a02de519bf230551b038633ff5237d15d0d
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-07-14 19:38:29 +0530

    added user models ,

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"log"
 	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/muhammedbilals/ecom-go/controllers"
-	"github.com/muhammedbilals/ecom-go/database"
-	"github.com/muhammedbilals/ecom-go/middleware"
+	// "github.com/muhammedbilals/ecom-go/controllers"
+	// "github.com/muhammedbilals/ecom-go/database"
+	// "github.com/muhammedbilals/ecom-go/middleware"
 	"github.com/muhammedbilals/ecom-go/routes"
 	// "github.com/muhammedbilals/ecom-go/tokens"
 )
@@ -18,17 +17,25 @@ func main(){
 		port ="8000"
 	}
 
-	app := controllers.NewApplication(database.ProductData(database.Client, "Products"),database.UserData(database.Client,"Users"))
+	// app := controllers.NewApplication(database.ProductData(database.Client, "Products"),database.UserData(database.Client,"Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
-	router.Use(middleware.Authentication())
+	// router.Use(middleware.Authentication())
 
-	router.GET("/addtocart", app.AddToCart())
-	router.GET("/removeitem", app.RemoveItem())
-	router.GET("/cartcheckout", app.CartCheckout())
-	router.GET("/instantbuy",app.InstantBuy())
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200,gin.H{"success":"Access granded for API-1"})
+	})
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200,gin.H{"success":"Access granded for API-2"})
+	})
+
+
+	// router.GET("/addtocart", app.AddToCart())
+	// router.GET("/removeitem", app.RemoveItem())
+	// router.GET("/cartcheckout", app.CartCheckout())
+	// router.GET("/instantbuy",app.InstantBuy())
 
 	log.Fatal(router.Run(":" + port))
 
--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	"github.com/muhammedbilals/ecom-go/controllers"
+	// "github.com/muhammedbilals/ecom-go/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRouts * gin.Engine){
-	incomingRouts.POST("/users/signup , ",controllers.SignUp())
-	incomingRouts.POST("/users/login , ",controllers.Login())
-	incomingRouts.POST("/admin/addproduct",controllers.AddProduct())
-	incomingRouts.GET("/users/productview , ",controllers.ProductView)
-	incomingRouts.GET("/users/search , ",controllers.Search())
+	// incomingRouts.POST("/users/signup , ",controllers.SignUp())
+	// incomingRouts.POST("/users/login , ",controllers.Login())
+	// incomingRouts.POST("/admin/addproduct",controllers.AddProduct())
+	// incomingRouts.GET("/users/productview , ",controllers.ProductView)
+	// incomingRouts.GET("/users/search , ",controllers.Search())
 }
