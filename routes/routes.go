542a48847e7d1068044b350905c10a5a0ed1bcbc
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-07-15 23:19:10 +0530

    created mongo db connection file

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,27 @@
 package routes
 
 import (
-	// "github.com/muhammedbilals/ecom-go/controllers"
+	"github.com/muhammedbilals/ecom-go/controllers"
+	"github.com/muhammedbilals/ecom-go/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRouts * gin.Engine){
+	incomingRouts.Use(middleware.Authenticate())
+	incomingRouts.GET("/users",controllers.GetUser())
+	incomingRouts.GET("/users/:user_id ",controllers.GetUser())
+
 	// incomingRouts.POST("/users/signup , ",controllers.SignUp())
 	// incomingRouts.POST("/users/login , ",controllers.Login())
 	// incomingRouts.POST("/admin/addproduct",controllers.AddProduct())
 	// incomingRouts.GET("/users/productview , ",controllers.ProductView)
 	// incomingRouts.GET("/users/search , ",controllers.Search())
+}
+
+func AuthRoutes(incomingRouts * gin.Engine){
+	incomingRouts.POST("/users/signup , ",controllers.Signup())
+	
+	incomingRouts.GET("/users/login , ",controllers.Login())
+
 }
